5e48237d7c9a9d295e62084a8783a21c182267f1
ubuntu <viralkansodiya167@gmail.com>
2023-10-11 06:29:34 +0000

    add condition in validation of to time

--- kersten_erpnext/kersten_erpnext/timesheet.py
+++ kersten_erpnext/kersten_erpnext/timesheet.py
@@ -6,17 +6,17 @@ def set_hours_and_to_time(self,method):
     for row in self.time_logs:
         if not row.time and row.activity_type != "Break Time":
             frappe.throw(f"#Row{row.idx}: Field Time is mandetory")
-        date_str = row.date
-        time_str = row.time
-        # Parse the date string into a datetime object
-        date_obj = datetime.strptime(str(date_str), "%Y-%m-%d")
+            date_str = row.date
+            time_str = row.time
+            # Parse the date string into a datetime object
+            date_obj = datetime.strptime(str(date_str), "%Y-%m-%d")
 
-        # Parse the time string into a time object
-        time_obj = datetime.strptime(str(time_str), "%H:%M").time()
+            # Parse the time string into a time object
+            time_obj = datetime.strptime(str(time_str), "%H:%M").time()
 
-        # Combine the date and time objects into a single datetime object
-        combined_datetime = datetime.combine(date_obj.date(), time_obj)
-        row.from_time = combined_datetime
+            # Combine the date and time objects into a single datetime object
+            combined_datetime = datetime.combine(date_obj.date(), time_obj)
+            row.from_time = combined_datetime
 
 
 @frappe.whitelist()       
